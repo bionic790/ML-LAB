c4a5ab40ef9d3c64a2863cbb54533eb4f75e7bc2
bionic790 <53811382+bionic790@users.noreply.github.com>
2021-04-28 14:19:52 +0530

    Update code.py

--- ID3 ALGO/code.py
+++ ID3 ALGO/code.py
@@ -1,121 +1,106 @@
+import pandas as pd
+df = pd.read_csv('PlayTennis.csv')
+print("\n Input Data Set is:\n", df)
+t = df.keys()[-1]
+print('Target Attribute is: ', t)
+# Get the attribute names from input dataset
+attribute_names = list(df.keys())
+#Remove the target attribute from the attribute names list
+attribute_names.remove(t) 
+print('Predicting Attributes: ', attribute_names)
+#Function to calculate the entropy of collection S
 import math
-import csv
-def load_csv(filename):
-    lines=csv.reader(open(filename,"r"));
-    dataset = list(lines)
-    headers = dataset.pop(0)
-    return dataset,headers
+def entropy(probs):  
+    return sum( [-prob*math.log(prob, 2) for prob in probs])
 
-class Node:
-    def __init__(self,attribute):
-        self.attribute=attribute
-        self.children=[]
-        self.answer=""
+#Function to calulate the entropy of the given Data Sets/List with 
+#respect to target attributes
+def entropy_of_list(ls,value):  
+    from collections import Counter
+    cnt = Counter(x for x in ls)# Counter calculates the propotion of class
+    print('Target attribute class count(Yes/No)=',dict(cnt))
+    total_instances = len(ls)  
+    print("Total no of instances/records associated with {0} is: {1}".format(value,total_instances ))
+    probs = [x / total_instances for x in cnt.values()]  # x means no of YES/NO
+    print("Probability of Class {0} is: {1:.4f}".format(min(cnt),min(probs)))
+    print("Probability of Class {0} is: {1:.4f}".format(max(cnt),max(probs)))
+    return entropy(probs) # Call Entropy
+
+def information_gain(df, split_attribute, target_attribute,battr):
+    print("\n\n-----Information Gain Calculation of ",split_attribute, " --------") 
+    df_split = df.groupby(split_attribute) # group the data based on attribute values
+    glist=[]
+    for gname,group in df_split:
+        print('Grouped Attribute Values \n',group)
+        glist.append(gname) 
+    
+    glist.reverse()
+    nobs = len(df.index) * 1.0   
+    df_agg1=df_split.agg({target_attribute:lambda x:entropy_of_list(x, glist.pop())})
+    df_agg2=df_split.agg({target_attribute :lambda x:len(x)/nobs})
+    
+    df_agg1.columns=['Entropy']
+    df_agg2.columns=['Proportion']
+    
+    # Calculate Information Gain:
+    new_entropy = sum( df_agg1['Entropy'] * df_agg2['Proportion'])
+    if battr !='S':
+        old_entropy = entropy_of_list(df[target_attribute],'S-'+df.iloc[0][df.columns.get_loc(battr)])
+    else:
+        old_entropy = entropy_of_list(df[target_attribute],battr)
+    return old_entropy - new_entropy
+
+def id3(df, target_attribute, attribute_names, default_class=None,default_attr='S'):
+    
+    from collections import Counter
+    cnt = Counter(x for x in df[target_attribute])# class of YES /NO
+    
+    ## First check: Is this split of the dataset homogeneous?
+    if len(cnt) == 1:
+        return next(iter(cnt))  # next input data set, or raises StopIteration when EOF is hit.
+    
+    ## Second check: Is this split of the dataset empty? if yes, return a default value
+    elif df.empty or (not attribute_names):
+        return default_class  # Return None for Empty Data Set
+    
+    ## Otherwise: This dataset is ready to be devied up!
+    else:
+        # Get Default Value for next recursive call of this function:
+        default_class = max(cnt.keys()) #No of YES and NO Class
+        # Compute the Information Gain of the attributes:
+        gainz=[]
+        for attr in attribute_names:
+            ig= information_gain(df, attr, target_attribute,default_attr)
+            gainz.append(ig)
+            print('Information gain of ',attr,' is : ',ig)
         
-def subtables(data,col,delete):
-    dic={}
-    coldata=[row[col] for row in data]
-    attr=list(set(coldata))
-    
-    counts=[0]*len(attr)
-    r=len(data)
-    c=len(data[0])
-    for x in range(len(attr)):
-        for y in range(r):
-            if data[y][col]==attr[x]:
-                counts[x]+=1
+        index_of_max = gainz.index(max(gainz))               # Index of Best Attribute
+        best_attr = attribute_names[index_of_max]            # Choose Best Attribute to split on
+        print("\nAttribute with the maximum gain is: ", best_attr)
+        # Create an empty tree, to be populated in a moment
+        tree = {best_attr:{}} # Initiate the tree with best attribute as a node 
+        remaining_attribute_names =[i for i in attribute_names if i != best_attr]
         
-    for x in range(len(attr)):
-        dic[attr[x]]=[[0 for i in range(c)] for j in range(counts[x])]
-        pos=0
-        for y in range(r):
-            if data[y][col]==attr[x]:
-                if delete:
-                    del data[y][col]
-                dic[attr[x]][pos]=data[y]
-                pos+=1
-    return attr,dic
-    
-def entropy(S):
-    attr=list(set(S))
-    if len(attr)==1:
-        return 0
-    
-    counts=[0,0]
-    for i in range(2):
-        counts[i]=sum([1 for x in S if attr[i]==x])/(len(S)*1.0)
-    
-    sums=0
-    for cnt in counts:
-        sums+=-1*cnt*math.log(cnt,2)
-    return sums
-
-def compute_gain(data,col):
-    attr,dic = subtables(data,col,delete=False)
-    
-    total_size=len(data)
-    entropies=[0]*len(attr)
-    ratio=[0]*len(attr)
-    
-    total_entropy=entropy([row[-1] for row in data])
-    for x in range(len(attr)):
-        ratio[x]=len(dic[attr[x]])/(total_size*1.0)
-        entropies[x]=entropy([row[-1] for row in dic[attr[x]]])
-        total_entropy-=ratio[x]*entropies[x]
-    return total_entropy
-
-def build_tree(data,features):
-    lastcol=[row[-1] for row in data]
-    if(len(set(lastcol)))==1:
-        node=Node("")
-        node.answer=lastcol[0]
-        return node
-    
-    n=len(data[0])-1
-    gains=[0]*n
-    for col in range(n):
-        gains[col]=compute_gain(data,col)
-    split=gains.index(max(gains))
-    node=Node(features[split])
-    fea = features[:split]+features[split+1:]
-
-    
-    attr,dic=subtables(data,split,delete=True)
-    
-    for x in range(len(attr)):
-        child=build_tree(dic[attr[x]],fea)
-        node.children.append((attr[x],child))
-    return node
-
-def print_tree(node,level):
-    if node.answer!="":
-        print("  "*level,node.answer)
-        return
-    
-    print("  "*level,node.attribute)
-    for value,n in node.children:
-        print("  "*(level+1),value)
-        print_tree(n,level+2)
-
-        
-def classify(node,x_test,features):
-    if node.answer!="":
-        print(node.answer)
-        return
-    pos=features.index(node.attribute)
-    for value, n in node.children:
-        if x_test[pos]==value:
-            classify(n,x_test,features)
-            
-'''Main program'''
-dataset,features=load_csv("data.csv")
-node1=build_tree(dataset,features)
-
-print("The decision tree for the dataset using ID3 algorithm is")
-print_tree(node1,0)
-testdata,features=load_csv("test.csv")
-
-for xtest in testdata:
-    print("The test instance:",xtest)
-    print("The label for test instance:",end="   ")
-    classify(node1,xtest,features)
+        # Split dataset-On each split, recursively call this algorithm.Populate the empty tree with subtrees, which
+        # are the result of the recursive call
+        for attr_val, data_subset in df.groupby(best_attr):
+            subtree = id3(data_subset,target_attribute, remaining_attribute_names,default_class,best_attr)
+            tree[best_attr][attr_val] = subtree
+        return tree
+from pprint import pprint
+tree = id3(df,t,attribute_names)
+print("\nThe Resultant Decision Tree is:")
+pprint(tree)
+def classify(instance, tree,default=None): # Instance of Play Tennis with Predicted    
+    attribute = next(iter(tree)) # Outlook/Humidity/Wind       
+    if instance[attribute] in tree[attribute].keys(): # Value of the attributs in  set of Tree keys  
+        result = tree[attribute][instance[attribute]]
+        if isinstance(result, dict): # this is a tree, delve deeper
+            return classify(instance, result)
+        else:
+            return result # this is a label
+    else:
+        return default
+df_new=pd.read_csv('PlayTennisTest.csv')
+df_new['predicted'] = df_new.apply(classify, axis=1, args=(tree,'?')) 
+print(df_new)
